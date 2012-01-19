5a8d42a286bdb8387589ed76381a33fb0caf83e5
Evgeny Reznichenko <kusakyky@gmail.com>
2012-01-19 11:35:46 +0400

    init 2

--- index.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-
-fs.readFile('./src/test.js', 'utf8', function (err, data) {
-	var pkg = data.replace(/[\r\n\t]/g, '').match(/\/\*pkg(.*)\*\//);
-	console.log(data);
-	console.log('pkg:\n' + pkg);
-});
